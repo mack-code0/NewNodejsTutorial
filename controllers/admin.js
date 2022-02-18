0474ebf89ca5653e268acb164a61d56b80dc6871
mack-code0 <macaulayhilbert202@gmail.com>
2022-02-18 23:04:52 +0100

    added middleware for auth

--- controllers/admin.js
+++ controllers/admin.js
@@ -5,7 +5,7 @@ exports.getAddProduct = (req, res, next) => {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     editing: false,
-    isAuthenticated: req.session.user
+    isAuthenticated: req.session.isLoggedIn
   });
 };
 
@@ -19,7 +19,7 @@ exports.postAddProduct = (req, res, next) => {
     price: price,
     description: description,
     imageUrl: imageUrl,
-    userId: req.session.user
+    userId: req.session.isLoggedIn
   });
   product
     .save()
@@ -49,7 +49,7 @@ exports.getEditProduct = (req, res, next) => {
         path: '/admin/edit-product',
         editing: editMode,
         product: product,
-        isAuthenticated: req.session.user
+        isAuthenticated: req.session.isLoggedIn
       });
     })
     .catch(err => console.log(err));
@@ -85,7 +85,7 @@ exports.getProducts = (req, res, next) => {
         prods: products,
         pageTitle: 'Admin Products',
         path: '/admin/products',
-        isAuthenticated: req.session.user
+        isAuthenticated: req.session.isLoggedIn
       });
     })
     .catch(err => console.log(err));
